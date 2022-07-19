82477a3405d12b7ebe39424f80bde1e548001e95
Scarletfire19 <89076186+Scarletfire19@users.noreply.github.com>
2022-07-19 21:03:19 +0800

    Update st2.py

--- st2.py
+++ st2.py
@@ -19,7 +19,7 @@ if uploaded_file is not None:
 
 Tools = st.selectbox("Choose your Tool", ['Euclidean','braycurtis','canberra','chebyshev']) 
 
-dfnext25=input.drop(columns=df.columns[0], axis=1, inplace=True)
+dfnext25=input.drop(columns=input.columns[0], axis=1, inplace=True)
 
 if Tools == "Euclidean":
   #euclidean
